7cd097744af87b4c6db088784918846e5ba0ea82
v-grigorev <120387438+v-grigorev@users.noreply.github.com>
2023-06-23 12:06:00 +0700

    Create CustomButton.java

---
+++ lab3/src/ui/gui/CustomButton.java
@@ -0,0 +1,52 @@
+package ui.gui;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
+import static ui.gui.UIDesigner.BUTTON_BACKGROUND;
+import static ui.gui.UIDesigner.BUTTON_BACKGROUND_PRESSED;
+
+public class CustomButton extends JButton {
+    private Color hoverBackgroundColor;
+    private Color pressedBackgroundColor;
+
+    public CustomButton()
+    {
+        this(null);
+    }
+
+    public CustomButton(String text) {
+        super(text);
+        super.setContentAreaFilled(false);
+        setBackground(BUTTON_BACKGROUND);
+        setPressedBackgroundColor(BUTTON_BACKGROUND_PRESSED);
+        setHoverBackgroundColor(BUTTON_BACKGROUND.brighter());
+        setForeground(Color.LIGHT_GRAY);
+        setFocusPainted(false);
+        setBorder(new LineBorder(Color.GRAY, 1));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        if(getModel().isPressed()) {
+            g.setColor(pressedBackgroundColor);
+        }
+        else if(getModel().isRollover()) {
+            g.setColor(hoverBackgroundColor);
+        }
+        else {
+            g.setColor(getBackground());
+        }
+        g.fillRect(0, 0, getWidth(), getHeight());
+        super.paintComponent(g);
+    }
+
+    public void setHoverBackgroundColor(Color hoverBackgroundColor) {
+        this.hoverBackgroundColor = hoverBackgroundColor;
+    }
+
+    public void setPressedBackgroundColor(Color pressedBackgroundColor) {
+        this.pressedBackgroundColor = pressedBackgroundColor;
+    }
+}
