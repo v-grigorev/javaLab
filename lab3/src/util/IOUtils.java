d9cf734feaf1f8d1f65d3eb68849a150f6207e78
v-grigorev <120387438+v-grigorev@users.noreply.github.com>
2023-06-23 12:23:36 +0700

    Create IOUtils.java

---
+++ lab3/src/util/IOUtils.java
@@ -0,0 +1,29 @@
+package util;
+
+public class IOUtils {
+    public static <T> void print(T message) {
+        if(System.console() != null) {
+            System.console().printf("%s", message.toString());
+            System.console().flush();
+        }
+        else {
+            System.out.printf("%s", message.toString());
+            System.out.flush();
+        }
+    }
+
+    public static <T> void println(T message) {
+        if(System.console() != null) {
+            System.console().printf("%s\n", message.toString());
+            System.console().flush();
+        }
+        else {
+            System.out.printf("%s\n", message.toString());
+            System.out.flush();
+        }
+    }
+
+    public static void println() {
+        println("");
+    }
+}
