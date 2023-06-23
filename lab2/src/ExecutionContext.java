3d9db7cb6691a83f0708588792c4b39808aa6d48
v-grigorev <120387438+v-grigorev@users.noreply.github.com>
2023-06-23 11:27:58 +0700

    Create ExecutionContext.java

---
+++ lab2/src/ExecutionContext.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ExecutionContext {
+    public static Stack<Double> stack = new Stack<>();
+    public static Map<String, Double> map = new HashMap<>();
+}
